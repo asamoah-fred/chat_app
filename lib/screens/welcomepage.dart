83ec458c37cee68da21ab163122c3b13fc8f8c04
asamoah-fred <asamoahgodfred42@gmail.com>
2022-03-10 20:33:43 +0000

    Set Background color

--- lib/screens/welcomepage.dart
+++ lib/screens/welcomepage.dart
@@ -12,11 +12,27 @@ class _WelcomePageState extends State<WelcomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        padding: EdgeInsets.symmetric(horizontal: 20),
+        padding: const EdgeInsets.symmetric(horizontal: 20),
         height: MediaQuery.of(context).size.height,
         decoration: BoxDecoration(
-          borderRadius: BorderRadius.all(
+          borderRadius: const BorderRadius.all(
             Radius.circular(5),
+          ),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.shade200,
+              offset: Offset(2, 4),
+              blurRadius: 5,
+              spreadRadius: 2,
+            ),
+          ],
+          gradient: const LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Color(0xfffbb448),
+              Color(0xffe46b10),
+            ],
           ),
         ),
       ),
