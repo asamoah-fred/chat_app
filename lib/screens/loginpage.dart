8ee260c932d09f738d0b07e605890f43b31041d3
asamoah-fred <asamoahgodfred42@gmail.com>
2022-03-11 03:12:55 +0000

    Back arrow

--- lib/screens/loginpage.dart
+++ lib/screens/loginpage.dart
@@ -333,47 +333,43 @@ class _LoginPageState extends State<LoginPage> {
                           ),
                         ),
                       ),
-                      //       ],
-                      //     ),
-                      //   ),
-                      // ),
-                      // Positioned(
-                      //   top: 40,
-                      //   left: 0,
-                      //   child: InkWell(
-                      //     onTap: () {
-                      //       Navigator.pop(context);
-                      //     },
-                      //     child: Container(
-                      //       padding: EdgeInsets.symmetric(
-                      //         horizontal: 10,
-                      //       ),
-                      //       child: Row(
-                      //         children: <Widget>[
-                      //           Container(
-                      //             padding: EdgeInsets.only(
-                      //               left: 0,
-                      //               top: 10,
-                      //               bottom: 10,
-                      //             ),
-                      //             child: Icon(
-                      //               Icons.keyboard_arrow_left,
-                      //               color: Colors.black,
-                      //             ),
-                      //           ),
-                      //           Text(
-                      //             'Back',
-                      //             style: TextStyle(
-                      //               fontSize: 15,
-                      //               fontWeight: FontWeight.w500,
-                      //             ),
-                      //           ),
-                      //         ],
-                      //       ),
-                      //     ),
-                      //   ),
-                      // ),
                     ],
+                  ),
+                ),
+              ),
+              Positioned(
+                top: 40,
+                left: 0,
+                child: InkWell(
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                  child: Container(
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 10,
+                    ),
+                    child: Row(
+                      children: <Widget>[
+                        Container(
+                          padding: EdgeInsets.only(
+                            left: 0,
+                            top: 10,
+                            bottom: 10,
+                          ),
+                          child: Icon(
+                            Icons.keyboard_arrow_left,
+                            color: Colors.black,
+                          ),
+                        ),
+                        Text(
+                          'Back',
+                          style: TextStyle(
+                            fontSize: 15,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ),
