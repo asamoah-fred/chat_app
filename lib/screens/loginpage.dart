b69141f7b362640e28c831448f390add550d0df4
asamoah-fred <asamoahgodfred42@gmail.com>
2022-03-11 03:05:17 +0000

    divider

--- lib/screens/loginpage.dart
+++ lib/screens/loginpage.dart
@@ -179,44 +179,44 @@ class _LoginPageState extends State<LoginPage> {
                           ),
                         ),
                       ),
-                      //         Container(
-                      //           margin: EdgeInsets.symmetric(
-                      //             vertical: 10,
-                      //           ),
-                      //           child: Row(
-                      //             children: const <Widget>[
-                      //               SizedBox(
-                      //                 width: 20,
-                      //               ),
-                      //               Expanded(
-                      //                 child: Padding(
-                      //                   padding: EdgeInsets.symmetric(
-                      //                     horizontal: 10,
-                      //                   ),
-                      //                   child: Divider(
-                      //                     thickness: 1,
-                      //                   ),
-                      //                 ),
-                      //               ),
-                      //               Text(
-                      //                 'or',
-                      //               ),
-                      //               Expanded(
-                      //                 child: Padding(
-                      //                   padding: EdgeInsets.symmetric(
-                      //                     horizontal: 10,
-                      //                   ),
-                      //                   child: Divider(
-                      //                     thickness: 1,
-                      //                   ),
-                      //                 ),
-                      //               ),
-                      //               SizedBox(
-                      //                 width: 10,
-                      //               ),
-                      //             ],
-                      //           ),
-                      //         ),
+                      Container(
+                        margin: EdgeInsets.symmetric(
+                          vertical: 10,
+                        ),
+                        child: Row(
+                          children: const <Widget>[
+                            SizedBox(
+                              width: 20,
+                            ),
+                            Expanded(
+                              child: Padding(
+                                padding: EdgeInsets.symmetric(
+                                  horizontal: 10,
+                                ),
+                                child: Divider(
+                                  thickness: 1,
+                                ),
+                              ),
+                            ),
+                            Text(
+                              'or',
+                            ),
+                            Expanded(
+                              child: Padding(
+                                padding: EdgeInsets.symmetric(
+                                  horizontal: 10,
+                                ),
+                                child: Divider(
+                                  thickness: 2,
+                                ),
+                              ),
+                            ),
+                            SizedBox(
+                              width: 10,
+                            ),
+                          ],
+                        ),
+                      ),
                       //         Container(
                       //           height: 50,
                       //           margin: EdgeInsets.symmetric(
