39c6f05ed59cd65e811845d3c4a3b90ebf2f5212
asamoah-fred <asamoahgodfred42@gmail.com>
2022-03-11 03:39:53 +0000

    Register Button

--- lib/screens/signup_page.dart
+++ lib/screens/signup_page.dart
@@ -218,6 +218,53 @@ class _SignUpPageState extends State<SignUpPage> {
                       SizedBox(
                         height: 20,
                       ),
+                      Container(
+                        width: MediaQuery.of(context).size.width,
+                        padding: EdgeInsets.symmetric(
+                          vertical: 15,
+                        ),
+                        alignment: Alignment.center,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(
+                              15,
+                            ),
+                          ),
+                          boxShadow: <BoxShadow>[
+                            BoxShadow(
+                              color: Colors.grey.shade200,
+                              offset: Offset(
+                                2,
+                                4,
+                              ),
+                              blurRadius: 5,
+                              spreadRadius: 2,
+                            )
+                          ],
+                          gradient: LinearGradient(
+                            begin: Alignment.centerLeft,
+                            end: Alignment.centerRight,
+                            colors: const [
+                              Color(
+                                0xfffbb448,
+                              ),
+                              Color(
+                                0xfff7892b,
+                              ),
+                            ],
+                          ),
+                        ),
+                        child: Text(
+                          'Register Now',
+                          style: TextStyle(
+                            fontSize: 20,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 30,
+                      ),
                     ],
                   ),
                 ),
