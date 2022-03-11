c47b43578ac3167f2959a7820e03dd5254089c9b
asamoah-fred <asamoahgodfred42@gmail.com>
2022-03-11 03:07:08 +0000

    Other Button

--- lib/screens/loginpage.dart
+++ lib/screens/loginpage.dart
@@ -217,75 +217,75 @@ class _LoginPageState extends State<LoginPage> {
                           ],
                         ),
                       ),
-                      //         Container(
-                      //           height: 50,
-                      //           margin: EdgeInsets.symmetric(
-                      //             vertical: 20,
-                      //           ),
-                      //           decoration: BoxDecoration(
-                      //             borderRadius: BorderRadius.all(
-                      //               Radius.circular(
-                      //                 10,
-                      //               ),
-                      //             ),
-                      //           ),
-                      //           child: Row(
-                      //             children: <Widget>[
-                      //               Expanded(
-                      //                 flex: 1,
-                      //                 child: Container(
-                      //                   decoration: BoxDecoration(
-                      //                     color: Color(0xff1959a9),
-                      //                     borderRadius: BorderRadius.only(
-                      //                       bottomLeft: Radius.circular(
-                      //                         5,
-                      //                       ),
-                      //                       topLeft: Radius.circular(
-                      //                         5,
-                      //                       ),
-                      //                     ),
-                      //                   ),
-                      //                   alignment: Alignment.center,
-                      //                   child: Text(
-                      //                     'f',
-                      //                     style: TextStyle(
-                      //                       color: Colors.white,
-                      //                       fontSize: 25,
-                      //                       fontWeight: FontWeight.w400,
-                      //                     ),
-                      //                   ),
-                      //                 ),
-                      //               ),
-                      //               Expanded(
-                      //                 flex: 5,
-                      //                 child: Container(
-                      //                   decoration: BoxDecoration(
-                      //                     color: Color(
-                      //                       0xff2872b5,
-                      //                     ),
-                      //                     borderRadius: BorderRadius.only(
-                      //                       bottomRight: Radius.circular(
-                      //                         5,
-                      //                       ),
-                      //                       topRight: Radius.circular(
-                      //                         5,
-                      //                       ),
-                      //                     ),
-                      //                   ),
-                      //                   alignment: Alignment.center,
-                      //                   child: Text(
-                      //                     'Log in with Facebook',
-                      //                     style: TextStyle(
-                      //                       color: Colors.white,
-                      //                       fontSize: 18,
-                      //                       fontWeight: FontWeight.w400,
-                      //                     ),
-                      //                   ),
-                      //                 ),
-                      //               ),
-                      //             ],
-                      //           ),
-                      //         ),
+                      Container(
+                        height: 50,
+                        margin: EdgeInsets.symmetric(
+                          vertical: 20,
+                        ),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(
+                              10,
+                            ),
+                          ),
+                        ),
+                        child: Row(
+                          children: <Widget>[
+                            Expanded(
+                              flex: 1,
+                              child: Container(
+                                decoration: BoxDecoration(
+                                  color: Color(0xff1959a9),
+                                  borderRadius: BorderRadius.only(
+                                    bottomLeft: Radius.circular(
+                                      5,
+                                    ),
+                                    topLeft: Radius.circular(
+                                      5,
+                                    ),
+                                  ),
+                                ),
+                                alignment: Alignment.center,
+                                child: Text(
+                                  'f',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 25,
+                                    fontWeight: FontWeight.w400,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Expanded(
+                              flex: 5,
+                              child: Container(
+                                decoration: BoxDecoration(
+                                  color: Color(
+                                    0xff2872b5,
+                                  ),
+                                  borderRadius: BorderRadius.only(
+                                    bottomRight: Radius.circular(
+                                      5,
+                                    ),
+                                    topRight: Radius.circular(
+                                      5,
+                                    ),
+                                  ),
+                                ),
+                                alignment: Alignment.center,
+                                child: Text(
+                                  'Log in with Facebook',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.w400,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
                       //         SizedBox(
                       //           height: 20,
                       //         ),
