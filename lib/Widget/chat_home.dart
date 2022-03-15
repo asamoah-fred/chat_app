1e5661b52db5208d9bc0b8ce7622237b7b152998
asamoah-fred <asamoahgodfred42@gmail.com>
2022-03-15 14:32:37 +0000

    Set Tabs

--- lib/Widget/chat_home.dart
+++ lib/Widget/chat_home.dart
@@ -1,12 +1,65 @@
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
-  const Home({ Key? key }) : super(key: key);
+  const Home({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return DefaultTabController(
+      length: 5,
+      child: Scaffold(
+        appBar: AppBar(
+          // elevation: 0,
+          backgroundColor: Colors.white,
+          title: const Text(
+            "facebook",
+            style: TextStyle(
+              fontSize: 25,
+              fontWeight: FontWeight.bold,
+              color: Colors.blue,
+            ),
+          ),
+          actions: <Widget>[
+            IconButton(
+              icon: const Icon(
+                Icons.search,
+                color: Colors.black,
+              ),
+              onPressed: () {},
+            ),
+            IconButton(
+              icon: const Icon(
+                Icons.message,
+                color: Colors.black,
+              ),
+              onPressed: () {},
+            ),
+          ],
+          //TabBar
+          bottom: const TabBar(
+            unselectedLabelColor: Colors.black54,
+            indicatorColor: Colors.blue,
+            labelColor: Colors.blue,
+            tabs: [
+              Tab(
+                icon: Icon(Icons.home),
+              ),
+              Tab(
+                icon: Icon(Icons.group),
+              ),
+              Tab(
+                icon: Icon(Icons.ondemand_video),
+              ),
+              Tab(
+                icon: Icon(Icons.notifications_none),
+              ),
+              Tab(
+                icon: Icon(Icons.menu),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
